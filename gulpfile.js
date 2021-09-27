3f2c98a2a4253e9f46d84bcc13694b83844ce95f
Tanguy Labrador Ruiz <tanguy.labrador.ruiz@gmail.com>
2021-09-27 15:20:09 +0200

    ajout  dossier template 2eme tentative

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,6 @@
-/**
- * Gulp Files
- */
-var gulp        = require('gulp'),
-    concat      = require('gulp-concat'),
-    prefix      = require('gulp-autoprefixer'),
-    sass        = require('gulp-sass'),
-    image       = require('gulp-image'),
-    uglify      = require('gulp-uglify');
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
 
-/**
- * Imagecompress - convert images into small size
- */
-gulp.task('Imagecompress', function(){
-        return gulp.src('assets/images-original/**/*')
-                .pipe(image())
-                .pipe(gulp.dest('assets/images'))
-});
-
-/**
- * SASS - Compile SASS files into CSS
- */
-gulp.task('SASSCompile',function(){
-    return gulp.src('assets/scss/style.scss')
-            .pipe(sass())
-            .pipe(prefix('last 2 versions'))
-            .pipe(concat('style.css'))
-            .pipe(gulp.dest('assets/css'))
-});
-
-/**
- * JsCompress - compile js vendors into one file
- */
-gulp.task('JsCompress',function(){
-  return gulp.src('assets/js/plugins/*.js')
-          .pipe(concat('plugins.js'))
-          .pipe(uglify())
-          .pipe(gulp.dest('source/assets/js'))
-});
-
-/**
- * Gulp Watch and Tasks
- */
-
- gulp.task('watch',function(){
-    gulp.watch('assets/images/**/*',gulp.series('Imagecompress'));
-    gulp.watch('assets/scss/*.scss',gulp.series('SASSCompile'));
-    gulp.watch('assets/js/plugins/*.js',gulp.series('JsCompress'));
- });
-
-// Gulp Tasks
- gulp.task('default',['watch']);
+exports.default = defaultTask
